80cb6edccd7f7a6dc473ce6cf6c041b9c0d5e39b
SidKop <tranterc@gmail.com>
2015-05-25 20:09:15 +0800

    new forms

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
-console.log("test");
+
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+
+if (computerChoice <0.33){
+    computerChoce = 'rock'
+}
+else if (computerChoice > 0.34 && computerChoice < 0.66){
+    computerChoice = "paper"
+}
+else{
+    computerChoice = 'scissors'
+}
+
+console.log(computerChoice)
