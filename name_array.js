a63c21f4abf661471c3b728d9ece74c6c4a5e84e
SidKop <tranterc@gmail.com>
2015-05-25 22:53:39 +0800

    update

---
+++ name_array.js
@@ -0,0 +1,24 @@
+/*jshint multistr:true */
+
+
+var text = 'gheiw ghjewigweh giwehgihgiwe ghw hvwho Chris newgowngkw fnewi chris jowepjf Chrit cniwo chrit'
+var myName = 'Chris'
+var hits=[]
+
+for (i = 0; i < text.length; i++){
+    if (text[i] === myName[0]){
+       for (a = i; a < (myName.length + i); a++){
+            if (text[a] === myName[a-i]){
+                hits.push(text[a]);
+            }
+       }
+    }
+}
+
+
+if (hits.length == 0){
+    console.log ("Your name wasn't found!");
+}
+else{
+    console.log(hits);
+}
