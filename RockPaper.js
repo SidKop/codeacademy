6a0fdd6e0e815991483e68f3f45be519f4685f32
SidKop <tranterc@gmail.com>
2015-05-26 13:52:31 +0800

    new files

--- RockPaper.js
+++ RockPaper.js
@@ -7,11 +7,13 @@ while ( userChoice !== 'rock' && userChoice !=='paper' && userChoice != 'scissor
 
 var computerChoice = Math.random();
 if (computerChoice < 0.34) {
-	computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
-	computerChoice = "paper";
-} else {
-	computerChoice = "scissors";
+    computerChoice = "rock";
+}
+else if(computerChoice <= 0.67) {
+	   computerChoice = "paper";
+}
+else {
+	   computerChoice = "scissors";
 } console.log("Computer: " + computerChoice);
 
 var result = compare(userChoice,computerChoice)
---
+++ loops.js
@@ -0,0 +1,20 @@
+// Write your code below!
+
+for (i=0; i<5; i++ ){
+    console.log("for loop");
+    i++
+}
+
+var foo = 0
+do{
+    console.log("do loop")
+    foo ++
+}
+while (foo < 4)
+
+
+foop = true
+while (foop === true){
+    console.log ("while loop")
+    foop = false
+}
