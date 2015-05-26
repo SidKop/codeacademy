bd7692ccbb7c9b31c6b0c9b8212e2b55a7d60c4a
SidKop <tranterc@gmail.com>
2015-05-26 16:25:23 +0800

    new files

---
+++ objects.js
@@ -0,0 +1,3 @@
+var foo = new Object()
+
+var aList = [[1,3,2],[3,2,'fewgw',33],[foo]]
---
+++ switch.js
@@ -0,0 +1,20 @@
+var user = prompt("Are you bored").toLowerCase();
+
+switch (user){
+    case 'yes':
+
+
+
+        console.log("shame")
+        break
+    case 'no':
+        console.log("that's lucky")
+        break
+    case 'maybe':
+        console.log("Well yes or no?")
+        break
+    default:
+        console.log("what sort of answer is that")
+        break
+
+}
